f53d6c9e541342bbbbd630492af7eba09e6f6940
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-01 11:29:09 -0400

    Make dataGenerator also location-agnostic

--- dataGenerator.py
+++ dataGenerator.py
@@ -2,12 +2,17 @@
 
 import json
 import re
+import os
 
 data = []
 counter = 0
 
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file_path)
+out_file = current_directory + "/pokemon.json"
+
 try:
-    with open("pokemon.json", "r") as json_file:
+    with open(out_file, "r") as json_file:
         data = json.load(json_file)
 except FileNotFoundError:
     data = []
@@ -23,7 +28,7 @@ while True:
         counter += 1
 
     except KeyboardInterrupt:
-        with open("pokemon.json", "w") as json_file:
+        with open(out_file, "w") as json_file:
             json.dump(data, json_file, indent=4)
         print(f"\nAdded {counter} entries")
         break
