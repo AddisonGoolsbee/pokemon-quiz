76904d9edb2a1c07bb16f3b4080b79e9b758563d
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-01-30 09:06:11 -0500

    Refactor the loading of data and create a 'pokemon' object

--- pokemonQuiz.py
+++ pokemonQuiz.py
@@ -5,11 +5,34 @@ import sys
 import tty
 import json
 
+
+class Pokemon:
+    def __init__(self, name, number, set):
+        self.name = name
+        self.number = number
+        self.set = set
+
+    def __repr__(self):
+        return self.name
+
+
 class Game:
     GENERATION_CUTOFFS = [0, 151, 251, 386, 493, 649, 721, 809]
 
     def __init__(self):
         self.current_hint = ""
+        self.pokemon = self.load_data()
+
+    def load_data(self):
+        current_file_path = os.path.abspath(__file__)
+        current_directory = os.path.dirname(current_file_path)
+        with open(current_directory + "/pokemon.json", "r") as json_file:
+            data = json.load(json_file)
+
+        res = []
+        for index, pokemon in enumerate(data):
+            res.append(Pokemon(pokemon["name"], index, pokemon["set"]))
+        return res
 
     # Choose settings and pokemon listing for the next quiz
     def setup(self):
@@ -32,45 +55,38 @@ class Game:
                     selection = input(
                         'Invalid option, please type one of the following: 1 2 3 4 5 6 7 all settings"\n'
                     )
-                    
-    
-    def run_quiz(self, pokemon_range: list):
-        current_file_path = os.path.abspath(__file__)
-        current_directory = os.path.dirname(current_file_path)
-        with open(current_directory + "/pokemon.json", "r") as json_file:
-            data = json.load(json_file)
 
+    def run_quiz(self, pokemon_range: list):
         num_pokemon = pokemon_range[1] - pokemon_range[0]
         num_correct = 0
         start = time.time()
 
-        listed_pokemon = []
+        answered_pokemon = []
 
-        for index, item in enumerate(data[pokemon_range[0] + 1 : pokemon_range[1] + 1], start=pokemon_range[0] + 1):
-            skip = True
-            if item["name"] in listed_pokemon:
-                skip = False
+        for index, pokemon in enumerate(
+            self.pokemon[pokemon_range[0] + 1 : pokemon_range[1] + 1],
+            start=pokemon_range[0] + 1,
+        ):
+            if pokemon.name not in answered_pokemon:
+                pokemon_set = self.get_pokemon_set(index)
+                while True:
+                    os.system("clear")
+                    self.print_listed_pokemon(answered_pokemon)
+                    print(f"{num_correct}/{num_pokemon}")
 
-            pokemon_set = self.get_pokemon_set(index, data)
-            while skip:
-                os.system("clear")
-                self.print_listed_pokemon(listed_pokemon)
-                print(f"{num_correct}/{num_pokemon}")
-
-                if self.validate_guess(f"{index}: ", pokemon_set):
-                    num_correct += len(pokemon_set)
-                    listed_pokemon += pokemon_set
-                    break
+                    if self.validate_guess(f"{index}: ", pokemon_set):
+                        num_correct += len(pokemon_set)
+                        answered_pokemon += pokemon_set
+                        break
 
         os.system("clear")
-        self.print_listed_pokemon(listed_pokemon)
+        self.print_listed_pokemon(answered_pokemon)
         print(f"{num_correct}/{num_pokemon}")
         print("Hooray!")
 
         total_time = time.time() - start
         minutes, seconds = divmod(total_time, 60)
         print(f"Time: {int(minutes)}:{int(seconds):02}")
-
 
     def validate_guess(self, prompt, answers):
         if self.current_hint:
@@ -113,7 +129,7 @@ class Game:
 
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    
+
     def handle_hint(self, input_str, answer):
         if len(input_str) == 5 and input_str.lower().strip() in ["help", "hint"]:
             if len(self.current_hint) < len(answer):
@@ -133,13 +149,13 @@ class Game:
             current_line_length += poke_length
 
         print()
-    
-    def get_pokemon_set(self, index: int, data: list):
-        pokemon_set = [data[index]["name"]]
-        if data[index]["set"]:
+
+    def get_pokemon_set(self, index: int):
+        pokemon_set = [self.pokemon[index].name]
+        if self.pokemon[index].set:
             temp_index = index + 1
-            while temp_index < len(data) and not data[temp_index]["set"]:
-                pokemon_set.append(data[temp_index]["name"])
+            while temp_index < len(self.pokemon) and not self.pokemon[temp_index].set:
+                pokemon_set.append(self.pokemon[temp_index].name)
                 temp_index += 1
 
         # print(data)
@@ -150,7 +166,7 @@ class Game:
         # exit()
 
         return pokemon_set
-    
+
     def check_if_correct(self, guess: str, pokemon_set: list):
         for name in pokemon_set:
             if guess[:5] == name[:5]:
